dc675e0317c2469cf79fa1322c8c2efd596f0ffa
donfack fred <freddydonfack7@gmail.com>
2023-08-21 17:37:56 +0100

    test des  structures

---
+++ pages/authentication/register_page.py
@@ -0,0 +1,29 @@
+from tkinter import *
+
+from pages.home.home_page import HomePage
+
+
+class RegisterPage:
+
+    def __init__(self,root,width,height):
+        # root = fenetre parent, width et height sont les dimensions de la fenetre
+        self.page = Canvas(root,width=width,height=height,bg="green")
+        Label(self.page,text="Register page").place(x=40,y=20)
+
+        # bouton de transition ver le register_page
+        #from pages.authentication.login_page import LoginPage
+        #Button(
+            #self.page, text="Se connecter",
+            #command=lambda: LoginPage(self.page, width=800, height=500)).place(x=90, y=60)
+
+        # bouton de transition ver le register_page approche 2
+        Button(
+        self.page, text="Se connecter",
+        command=self.page.destroy).place(x=90, y=60)
+
+        Button(
+            self.page, text="Go to home",
+            command=lambda :HomePage(self.page, width=800, height=500)).place(x=90, y=120)
+
+        self.page.place(x=0,y=0)
+
---
+++ pages/home/home_page.py
@@ -0,0 +1,17 @@
+from tkinter import *
+
+class HomePage:
+    def __init__(self,root,width,height):
+        # root = fenetre parent, width et height sont les dimensions de la fenetre
+        self.page = Canvas(root,width=width,height=height,bg="yellow")
+        Label(self.page,text="Home page").place(x=40,y=20)
+
+        #bouton de transition ver le register_page
+        Button(
+            self.page, text="Back button ",
+               command=self.page.destroy).place(x=90,y=60)
+
+
+        self.page.place(x=0,y=0)
+
+

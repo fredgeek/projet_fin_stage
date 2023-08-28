97384dd07ea41943684f0c4422e4bf4d7f17694e
donfack fred <freddydonfack7@gmail.com>
2023-08-28 09:38:25 +0100

    color finish

--- pages/home/boutton_compte.py
+++ pages/home/boutton_compte.py
@@ -9,23 +9,23 @@ class mon_compte:
         self.width=width
         self.height=height
         self.fonts=Font(family="Helvetica",size=13,underline=True,slant="italic",weight="bold")
-        self.page=Canvas(root,width=self.width,height=self.height,bg="#555555")
+        self.page=Canvas(root,width=self.width,height=self.height,bg="#7fc3c0")
 
         Label(self.page, text="consulter vos informations personelles: ",font=self.fonts
-              ,bg="#555555").place(x=self.width/4,y=4)
+              ,bg="#7fc3c0").place(x=self.width/4,y=4)
 
-        Label(self.page,text="NOM et PRENOM : ",font=self.fonts,bg="#555555").place(x=50,y=200)
+        Label(self.page,text="NOM et PRENOM : ",font=self.fonts,bg="#7fc3c0").place(x=50,y=200)
         nom=" username_nom"
         prenom=" username_prenom"
-        Label(self.page,text=f"{nom} {prenom}" ,font=("arial",12,"roman"),bg="#555555").place(x=250,y=200)
+        Label(self.page,text=f"{nom} {prenom}" ,font=("arial",12,"roman"),bg="#7fc3c0").place(x=250,y=200)
 
-        Label(self.page, text="Email : ",font=self.fonts,bg="#555555").place(x=50, y=300)
+        Label(self.page, text="Email : ",font=self.fonts,bg="#7fc3c0").place(x=50, y=300)
         email = " username_email"
-        Label(self.page, text=f"{email} ",font=("arial",12,"roman"),bg="#555555").place(x=250, y=300)
+        Label(self.page, text=f"{email} ",font=("arial",12,"roman"),bg="#7fc3c0").place(x=250, y=300)
 
-        Label(self.page, text="Mot de passe : ",font=self.fonts,bg="#555555").place(x=50, y=400)
+        Label(self.page, text="Mot de passe : ",font=self.fonts,bg="#7fc3c0").place(x=50, y=400)
         password = " username_password"
-        Label(self.page, text=f"{password} ",font=("arial",12,"roman"),bg="#555555").place(x=250, y=400)
+        Label(self.page, text=f"{password} ",font=("arial",12,"roman"),bg="#7fc3c0").place(x=250, y=400)
 
 
 
